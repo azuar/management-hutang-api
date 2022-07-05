f45edc6048b88aebae2fcc51695ad827b44d46cf
Mr_Azuar <azuarok@gmail.com>
2022-07-05 14:39:09 +0700

    initial

---
+++ controller/users.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const connection = require('../serverConfig');
+const router = express.Router();
+const crypto = require("crypto")
+
+router.get('/users', (req, res) => {
+    try {
+        let query = `SELECT * FROM users`;
+        connection.query(query, (error, result) => {
+            if (error) {
+                return res.json({
+                    errno: error.errno,
+                    message: error.message,
+                })
+            }
+
+            res.json(result)
+        })
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+router.post("/register/buyer", async(req, res) => {
+    try {
+        const data = req.body;
+        if (req.body.password) {
+            req.body.password = crypto.createHash("sha256").update(req.body.password).digest("hex");
+        }
+        let query = `INSERT INTO users VALUES (DEFAULT, '${req.body.username}', '${req.body.password}', User, '${req.body.username}')`;
+        connection.query(query, (error, result) => {
+            if (error) {
+                return res.json(error);
+            }
+
+            res.json("Success");
+        })
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors');
+const env = require('dotenv');
+const bodyParser = require('body-parser');
+const usersController = require('./controller/users');
+// const YAML = require('yamljs');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = YAML.load('./swagger/auth.yaml');
+const app = express();
+const port = env.PORT || 3000;
+
+app
+    .use(cors())
+    .use(bodyParser.json())
+    .use(express.urlencoded({ extended: true }))
+    .use('/api', usersController)
+    // .use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    .listen(port, () => {
+        console.log(`Listen to http://localhost:${port}`)
+    })
+
+app
+    .get('/', (req, res) => {
+        res.json('Active')
+    })
