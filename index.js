e98f2882434982176eadcf5475d554081dea2a3c
rijalelfikr1 <rijalelfikri8@gmail.com>
2022-07-19 22:49:05 +0700

    oke

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express')
-const cors = require('cors');
-const env = require('dotenv');
-const bodyParser = require('body-parser');
-const usersController = require('./controller/users');
-const hutangController = require('./controller/hutang');
-const preOrderController = require('./controller/pre-orders');
+const express = require("express");
+const cors = require("cors");
+const env = require("dotenv");
+const bodyParser = require("body-parser");
+const usersController = require("./controller/users");
+const hutangController = require("./controller/hutang");
+const preOrderController = require("./controller/pre-orders");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
-    .use(cors())
-    .use(bodyParser.json())
-    .use(express.urlencoded({ extended: true }))
-    .use('/api', usersController, hutangController, preOrderController)
-    .listen(port, () => {
-        console.log(`Listen to http://localhost:${port}`)
-    })
+  .use(cors())
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: true }))
+  .use("/api", usersController, hutangController, preOrderController)
+  .listen(port, () => {
+    console.log(`Listen to http://localhost:${port}`);
+  });
 
-app
-    .get('/', (req, res) => {
-        res.json('Active')
-    })
+app.get("/", (req, res) => {
+  res.json("Active");
+});
