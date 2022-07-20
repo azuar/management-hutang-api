dae6f8fce877532cff9645be7a90ac64079403e6
Inforsys Developer <azuarok@gmail.com>
2022-07-20 11:40:40 +0700

    modifed throw error to json

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ const usersController = require("./controller/users");
 const hutangController = require("./controller/hutang");
 const preOrderController = require("./controller/pre-orders");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 300
 
 app
   .use(cors())
-  .use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-  })
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: true }))
   .use("/api", usersController, hutangController, preOrderController)
